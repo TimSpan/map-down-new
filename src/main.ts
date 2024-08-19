49ed5cd010f5afaee87a74d0c65ae16db9e9c587
TimSpan <cat20201314@163.com>
2024-08-19 15:10:27 +0800

    。。
    。。

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
-
+import DataVVue3 from '@kjgl77/datav-vue3'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(DataVVue3)
+app.mount('#app')
